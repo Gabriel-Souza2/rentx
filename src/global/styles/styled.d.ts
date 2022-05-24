f8ebbbd9f53b9dbd70237ec3135520650294ef9a
Gabriel Souza <gabriel.souza2@hotmail.com.br>
2022-05-24 19:06:19 -0300

    Configurando tema global

---
+++ src/global/styles/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+import theme from './theme';
+
+declare module 'style-components' {
+    type ThemeType = typeof theme;
+
+    export interface DefaultTheme extends ThemeType {}
+
+}
