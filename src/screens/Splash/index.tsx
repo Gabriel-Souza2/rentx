f337ae3269624860e7f454bd375456a210347c1d
Gabriel de Souza Gomes <gabriel@pop-os.localdomain>
2022-06-25 17:54:13 -0300

    Criando animação de splash screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,38 +1,75 @@
-import React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
 import Animated, { 
+    Extrapolate,
+    interpolate,
+    runOnJS,
     useAnimatedStyle, 
     useSharedValue,
+    withSequence,
     withTiming
 } from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/native';
 
 import {
     Container
 } from './styles';
 
-export function Splash() {
-    const animation = useSharedValue(0);
+import BrandSvg from '../../assets/brand.svg'
+import LogoSvg from '../../assets/logo.svg'
 
-    const animatedStyles = useAnimatedStyle(() => {
+export function Splash() {
+    const navigator = useNavigation();
+
+    const breandValue = useSharedValue(0);
+    const logoValue = useSharedValue(0);
+
+    const brandAnimation = useAnimatedStyle(() => {
         return {
-            transform: [{ translateX: withTiming(animation.value * 255)}]
+            opacity: interpolate(breandValue.value, 
+                [0, 50], 
+                [0, 1],
+                Extrapolate.CLAMP
+            ),
         }
+    });
+
+    const logoAnimation = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(logoValue.value, 
+                [0, 50], 
+                [0, 1],
+                Extrapolate.CLAMP
+            ),
+        }
+    });
+
+    function startApp() {
+        navigator.navigate('Home');
+    }
+
+    useEffect(() => {
+        breandValue.value = withSequence(
+            withTiming(50, {duration: 3500}),
+            withTiming(0, {duration: 3500}, () => {
+                logoValue.value = withSequence( 
+                    withTiming(50, {duration: 3500}),
+                    withTiming(0, {duration: 3500}, () => {
+                        'workelt'
+                        runOnJS(startApp)();
+                    })
+                )
+            })
+        );
     });
 
     return (
         <Container>
-
-            <Animated.View style={[styles.box, animatedStyles]} />
-            <Button onPress={() => { animation.value = Math.random() }} title="Move"/>
-
+            <Animated.View style={brandAnimation}>
+                <BrandSvg />
+            </Animated.View>
+            <Animated.View style={logoAnimation}>
+                <LogoSvg />
+            </Animated.View>
         </Container>
     );
 }
-
-const styles = StyleSheet.create({
-    box: {
-        height: 50,
-        width: 50,
-        backgroundColor: '#ff0000'
-    }
-})
